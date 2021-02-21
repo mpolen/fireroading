0d451ef5aecc89e437656c92b17def6ee18b85ee
Ashar <afarooq@mit.edu>
2021-02-21 16:37:39 -0500

    updated directories

--- pages/numbersMain.jsx
+++ pages/numbersMain.jsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import axios from "axios";
-import NumbersSelected from './NumbersSelected';
+import NumbersSelected from './numbersSelected';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardActionArea from '@material-ui/core/CardActionArea';
